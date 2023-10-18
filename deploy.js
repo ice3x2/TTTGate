0bd92c0c74e3fa90f51d3e4445f828c335efbe02
beom <ice3x2@gmail.com>
2023-10-18 15:39:37 +0900

    단순 - deploy 복구

--- deploy.js
+++ deploy.js
@@ -7,9 +7,8 @@ const { exec, execSync} = require('child_process');
 rimraf.sync(Path.join(process.cwd(), 'dist'));
 fs.mkdirSync(Path.join(process.cwd(), 'dist'));
 
-//todo 아래 두 줄 임시로 막아놓는다.
-//execSync(`npm --prefix ${Path.join(process.cwd(), 'admin')} run build`);
-//fsEx.moveSync(Path.join(process.cwd(), 'web'), Path.join(process.cwd(), 'dist', 'web'), { overwrite: true });
+execSync(`npm --prefix ${Path.join(process.cwd(), 'admin')} run build`);
+fsEx.moveSync(Path.join(process.cwd(), 'web'), Path.join(process.cwd(), 'dist', 'web'), { overwrite: true });
 
 execSync(`npm --prefix ${Path.join(process.cwd())} run build`);
 execSync(`npm --prefix ${Path.join(process.cwd())} run pkg`);
