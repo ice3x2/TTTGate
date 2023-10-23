e672e372a434de6621aaddeb1038c719cbbe13a0
beom <ice3x2@gmail.com>
2023-10-23 17:56:30 +0900

    LogWriter 작성중

--- src/util/logger/LogWriter.ts
+++ src/util/logger/LogWriter.ts
@@ -1,18 +1,99 @@
+import {tls} from "node-forge";
+import {Level, WriteConfig} from "./LoggerConfig"
+import fs from "fs";
+import Path from "path";
 
 interface LogMessage {
     name: string;
     module: string;
     message: string;
-    date: Date;
-    error: Error;
+    time: number;
+    level: Level;
+    error?: Error;
 }
 
 class LogWriter {
 
+    private readonly _name : string;
+    private  _filePath : string;
+    private readonly _dirPath : string;
     private _logMessageQueue : Array<LogMessage> = [];
-    private _logFileStream:
+    private _logFileStream : fs.WriteStream | undefined;
+    private readonly _history : number;
+    private readonly _fileWrite : boolean;
+    private readonly _consoleWrite : boolean;
+    private _todayDate : number = 0;
+
+
+
+    public static create(writeConfig: WriteConfig) : LogWriter {
+        return new LogWriter(writeConfig);
+    }
+
+    private constructor(writeConfig: WriteConfig) {
+        this._name = writeConfig.name;
+        this._dirPath = writeConfig.path!;
+        this._history = writeConfig.history ?? 30;
+        this._fileWrite = writeConfig.file ?? true;
+        this._consoleWrite = writeConfig.console ?? true;
+    }
+
+    private init() {
+        this._todayDate = new Date().getDate();
+        this._filePath = this.makeLogFilePath();
+
+    }
+
+    private sweepOldFiles() {
+        this._todayDate = new Date().getDate();
+
+
+    }
+
+    private logFilesInDir() : Array<string> {
+        let stat = fs.statSync(this._dirPath);
+        if(!stat.isDirectory())
+            throw new Error('Log directory is not directory.');
+        let files = fs.readdirSync(this._dirPath);
+        return files.filter((file) => {
+            let regexString = `^${this._name}-\\d{4}\\.\\d{2}\\.\\d{2}\\.log$`;
+            let regex = new RegExp(regexString);
+            return regex.test(file);
+        });
+
+    }
+
+    private makeLogFilePath() : string {
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let dayOfMonth = date.getDate();
+        return Path.join(this._dirPath, `${this._name}"-"${year}.${month < 10 ? '0' + month : month}.${dayOfMonth < 10 ? '0' + dayOfMonth : dayOfMonth}.log`);
+    }
+
+
+    public static daysSinceStartOfYear(): number {
+        const today: Date = new Date();
+        const startOfYear: Date = new Date(today.getFullYear(), 0, 1);
+        const millisecondsInDay: number = 24 * 60 * 60 * 1000; // 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
+        const diff: number = today.getTime() - startOfYear.getTime();
+        const daysPassed: number = Math.floor(diff / millisecondsInDay);
+        return daysPassed;
+    }
+
+    private pushMessage(message: LogMessage) {
+        this._logMessageQueue.push(message);
+
+    }
+
+
+
+
 
 
 }
 
-export default LogWriter;
+
+console.log(LogWriter.daysSinceStartOfYear());
+
+export { LogWriter, LogMessage};
--- src/util/logger/Logger.ts
+++ src/util/logger/Logger.ts
@@ -1,8 +1,57 @@
 
+import {Level}  from "./LoggerConfig";
+import {LogMessage } from "./LogWriter";
+
+interface OnMessage {
+    (message: LogMessage) : void;
+}
+
 class Logger {
-    private static _instance : Logger;
+    private readonly _onMessage : OnMessage;
+    private readonly _name : string = '';
+    private readonly _module : string = '';
+
+    private appendMessage(level: Level, message: string, e?: Error)  {
+        let logMessage : LogMessage =  {
+            name: this._name,
+            module: this._module,
+            message: message,
+            time: Date.now(),
+            level: level,
+            error:e
+        };
+        this._onMessage(logMessage);
+    }
 
 
+    public static create(name: string, module: string, onMessage: OnMessage) : Logger {
+        return new Logger(onMessage, name, module);
+    }
+
+
+
+    private constructor(onMessage: OnMessage, name: string, module: string) {
+        this._onMessage = onMessage;
+        this._name = name;
+        this._module = module;
+
+    }
+
+    public debug(message: string) : void {
+        this.appendMessage('debug', message);
+    }
+
+    public info(message: string) : void {
+        this.appendMessage('info', message);
+    }
+
+    public warn(message: string, e?: Error) : void {
+        this.appendMessage('warn', message);
+    }
+
+    public error(message: string, e?: Error) : void {
+        this.appendMessage('error', message, e);
+    }
 
 
 }
